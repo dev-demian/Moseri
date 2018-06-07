e9d403ac1e4ced2a6cab43186a4362238d3db294
jaehun <guak908@hanmail.net>
2018-06-07 20:13:18 +0900

    add Home Page

---
+++ Moseri/src/main/webapp/res/js/slider.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+
+            $('.multi-items').slick({
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                adaptiveHeight: true,
+                autoplay: true,
+                autoplayspeed: 800,
+                dots: true,
+                arrows:true,
+
+            })
+        });
